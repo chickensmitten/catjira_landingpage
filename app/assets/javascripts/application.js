99c5dd9cdbe8c41248242e6cd66bb82d6f971afb
chickensmitten <chia.shengyeong@gmail.com>
2015-06-21 08:35:59 +0800

    my designs before zane

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
+//= require jquery.zoom-scroller
+//= require theme
 //= require_tree .
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module CatjiraLandingPage
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # necessary for sprockets to call fonts from vendor
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
   end
 end
